bd0ed631f5d953455c64d5d24c3d626868773896
Ikechukwu Ohanugodu <ikechukwuandrew4@gmail.com>
2024-09-13 08:37:47 +0100

    Fixes on Environment Variables

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,13 +1,13 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "My FastAPI Project"
-    MONGO_URI: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "lassodDB"
-    SECRET_KEY: str = "jhdcvjhZDCygsdgfo7EW79ETE8GFYURFADUADF78rap9757p7349794tgirgfjhvdfjhvdcjxjvLZVse80r07q37r04r7-4r4rryegofuoefudvfyfyasdfvdyfvdyvfsyuvfusdfvy"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
+    PROJECT_NAME: str
+    MONGO_URI: str
+    DATABASE_NAME: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     class Config:
         env_file = ".env"
 
